08555c17b4f1306b5312e44ba167c6ec3d250835
TesnSai <shivamchaudhary8476@gmail.com>
2024-02-27 01:08:08 +0530

    built searchbox functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,13 @@
 import FoodCard from "./FoodCard";
+import Shimmer from "./Shimmer";
 import { useEffect, useState } from "react";
 import { SWIGGYAPI } from "../utils/constants";
 
 const Body = () => {
+
     const [list, setList] = useState([]);
+    const [search, setSearch] = useState("");
+
     useEffect(()=> {
         async function fetchData(){
             const response = await fetch(SWIGGYAPI);
@@ -13,15 +17,31 @@ const Body = () => {
         }
         fetchData();
     }, []);
-    //without dependency array in useEffect, the application goes into infinte loop.
-    return (
+    //without dependency array in useEffect, the application goes into infinte loop why?.
+
+    function searchOnClickHandler(){
+        console.log(search)
+        const updatedResList = list.filter((res) => res?.info?.name.toLowerCase().includes(search.toLowerCase()));
+        console.log(updatedResList);
+        setList(updatedResList);
+    }
+    return list.length===0 ? <Shimmer/> : (
         <div className="bodyContainer">
             <div className="heading">
                 <h2>Featured items in your area</h2>
                 <button className="filter" onClick={()=> {
                     const newList = list.filter((res) => res.info.avgRating > 4.2)
                     setList(newList);
-                }}>Show Restraunts with high ratings</button>
+                    }}>
+                    Show Restraunts with high ratings
+                </button>
+                <input type="text" onChange={(e) => {
+                    setSearch(e.target.value);
+                    }}>
+                </input>
+                <button onClick={()=> searchOnClickHandler()}>
+                    Search
+                </button>
             </div>
             <div className="cardsContainer">
                 {list.map((restrauntInfo, index) => <FoodCard key={index} restraunt={restrauntInfo}></FoodCard>)}
