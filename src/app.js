8183a4117fdc3026e6100f9d288814dac70a78b0
TesnSai <shivamchaudhary8476@gmail.com>
2024-02-27 13:05:39 +0530

    created basic routes using react router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => (
     <>
@@ -11,5 +15,21 @@ const AppLayout = () => (
         <Footer></Footer>
     </>
 )
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout/>,
+        errorElement: <Error/>
+    },
+    {
+        path: "/about",
+        element: <About/>
+    },
+    {
+        path: "/contact",
+        element: <Contact/>
+    },
+])
 const root = ReactDom.createRoot(document.getElementById("root"))
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Us Page</h1>
+            <h2>surajprakash96399@gmail.com</h2>
+        </div>
+    )
+}
+
+export default Contact;
